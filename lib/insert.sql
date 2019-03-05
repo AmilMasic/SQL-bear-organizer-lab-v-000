27f2e8403d58acb3d60aa2ef798f6ce0d2140604
Amil Masic <amil.masic@gmail.com>
2019-03-05 19:45:44 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO bears(name, age, gender, color, alive) VALUES ("Mr. Chocolate", 3, "m", "brown", "yes");
 INSERT INTO bears(name, age, gender, color, alive) VALUES ("Rowdy", 1, "m", "black", "yes");
-INSERT INTO bears(name, age, gender, color, alive) VALUES ("Tabitha", 6, "f", "black", "no");
+INSERT INTO bears(name, age, gender, color, alive) VALUES ("Tabitha", 6, "f", "black", "yes");
 INSERT INTO bears(name, age, gender, color, alive) VALUES ("Sergeant Brown", 9, "m", "brown", "yes");
 INSERT INTO bears(name, age, gender, color, alive) VALUES ("Melissa", 5, "f", "white" , "yes");
 INSERT INTO bears(name, age, gender, color, alive) VALUES ("Grinch", 10, "m", "brown", "no");
